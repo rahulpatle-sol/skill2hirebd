6313ef84ce0681d569c508145da8f9a736db5ce3
rahulpatle-sol <patler044@gmail.com>
2026-01-28 21:06:18 +0530

    updated cors issue

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ const app = express();
 
 // CORS mein dono port allow kar do (5173 admin aur 5174 user)
 app.use(cors({ 
-    origin: ["http://localhost:5173", "http://localhost:5174"], 
+    origin: ["http://localhost:5173", "http://localhost:5174",
+        "https://skill2-hire.vercel.app","https://ecojobboard.vercel.app/"
+    ], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
